152a67f3f3a5810132580c56896ad65d07881865
lisb3th21 <epoaquizacango@cifpfbmoll.eu>
2022-12-01 18:49:42 +0100

    fix error en la hora

--- app/src/main/java/com/lisbeth/alertsanddates/ChoseDateTime.java
+++ app/src/main/java/com/lisbeth/alertsanddates/ChoseDateTime.java
@@ -12,8 +12,8 @@ import android.widget.Toast;
 import java.util.Calendar;
 
 public class ChoseDateTime extends AppCompatActivity {
-    DatePickerDialog picker;
-    TimePickerDialog picker1;
+    private DatePickerDialog picker;
+    private TimePickerDialog picker1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +29,6 @@ public class ChoseDateTime extends AppCompatActivity {
         picker = new DatePickerDialog(this,
                 (view1, year1, monthOfYear, dayOfMonth) -> Toast.makeText(ChoseDateTime.this, dayOfMonth + "/" + (monthOfYear + 1) + "/" + year1, Toast.LENGTH_SHORT).show(), year, month, day);
         picker.show();
-
     }
 
     public void time(View view) {
@@ -38,15 +37,16 @@ public class ChoseDateTime extends AppCompatActivity {
         int minute = cldr.get(Calendar.MINUTE);
 
         // date picker dialog
+
+
         picker1 = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
             @Override
             public void onTimeSet(TimePicker timePicker, int i, int i1) {
-                Toast.makeText(ChoseDateTime.this, houre+"h"+minute, Toast.LENGTH_SHORT).show();
+                Toast.makeText(ChoseDateTime.this, i+"h"+i1, Toast.LENGTH_SHORT).show();
+
             }
         }, houre, minute, DateFormat.is24HourFormat(this));
         picker1.show();
-
-
     }
 
 }
